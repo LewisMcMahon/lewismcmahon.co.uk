dee10a3e0c5e67976c326a8c3a401ddf9db5faa4
Lewis McMahon <lewis@lewismcmahon.co.uk>
2015-05-13 23:54:10 +0100

    basic styling done

--- cont/home.php
+++ cont/home.php
@@ -1,39 +1,76 @@
+<?php 
 
+	$allSections =  section::getAllSectionsOrdered();
+	
+	print "<pre>";
+	print_r($allSections);
+	print "</pre>";
 
+	
+	foreach($allSections as $section){			
+		?>
 			<div class="container_12 section">
-			
-				<a name="contact"></a>
+				<a name="<?php print $section->getName();?>"></a>
+	
+				<div class="grid_12">
 				
-				<div class="grid_12 section-top section-bottom">
-				
-					<?php 
-
-						$sectionTest =  section::getAllSectionsOrdered();
-						
-						print "<pre>";
-						print_r($sectionTest);
-						print "</pre>";
+					<div class="padding">			
 					
-					?>
-									
-						<div class="padding">			
-							
-							<h2>Contact</h2>
-							
-							<p>
-								
-								<a href="mailto:lewis@lewismcmahon.co.uk">lewis@lewismcmahon.co.uk</a>
-								
-							</p>
-							
-							<p>
-								
-								0791 369 1883
-								
-							</p>
-							
-						</div>	
+						<h2><?php print $section->getName();?></h2>
 						
-				</div>
+					</div>	
 				
+				</div>
+				<?php foreach($section->getEntrys() as $entry){?>
+					<div class="container_12">	
+						<div class="grid_4">
+							
+								<div class="padding">			
+									
+									<h3><?php print $entry->getName(); ?></h3>
+									
+									<?php 
+									
+										if (count ($entry->getImages()) > 0){
+											print $entry->gettext();										
+										}
+									
+									
+									?>
+									
+								</div>						
+								
+								
+						</div>
+						
+						<div class="grid_8">
+						
+							<?php 
+									
+								if (count ($entry->getImages()) > 0){
+									
+									foreach ($entry->getImages() as $filename){
+										?> 
+											<a data-lightbox="<?php print $entry->getName(); ?>" href="img/<?php print $filename;?>">
+												<img  src="img/<?php print $filename;?>" />
+											</a>
+										<?php 
+									}
+									
+								}
+								else{
+									print $entry->gettext();
+								}
+							
+							
+							?>
+							
+						</div>
+						
+					</div>
+					
+				<?php }?>
 			</div>
+		<?
+	}
+?>
