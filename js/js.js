5ed53521ed89c9d3673a710614069ff5bfe9f401
Lewis McMahon <lewis@lewismcmahon.co.uk>
2016-04-26 19:17:50 +0100

    work boxes scaling now

--- js/js.js
+++ js/js.js
@@ -1,13 +1,11 @@
 function workBoxSizeSet(){
 	//resixe the work boxes and fill the text
-	$(".work-header").each(function (index) {
+	$(".work-container").each(function (index) {
 		//set the work header
-		$(this).height($(this).width() * 0.5);
+		$(".work-header",this).height($(".work-header",this).width() * 0.5);
 
-		//set the work header overlay (it must be set not just 100% for text fill)
-		$(".work-header-overlay",this).height($(this).height());
-		$(".work-header-overlay",this).width($(this).width());
-		$(".work-header-overlay h3",this).css("line-height",$(this).height()+"px");
+		//hack to center the title text
+		$(".work-header-overlay h3",this).css("line-height",$(".work-header",this).height()+"px");
 	});
 	$(".work-header-overlay h3").fitText(1.5);
 }
@@ -18,4 +16,27 @@ $( document ).ready(function() {
 
 $( window ).resize(function() {
 	workBoxSizeSet()
-});
+})
+
+
+$(".work-container" ).mouseenter(function () {
+	var marginToAdd = ($(".work-container").height() - $(".work-content",this).height()) / 2
+	if (marginToAdd < 0){
+		marginToAdd = 0;
+		$(".work-header",this).height($(".work-content",this).height())
+		$(".work-content",this).css("padding-top","10px")
+		$(".work-content",this).css("padding-bottom","10px")
+	}else{
+		$(".work-content",this).css("padding-top",marginToAdd+"px")
+		$(".work-content",this).css("padding-bottom",marginToAdd+"px")
+	}
+	$("h3",this).hide()
+	$(".work-content",this).fadeIn()
+
+})
+
+$(".work-container" ).mouseleave(function () {
+	$("h3",this).fadeIn()
+	$(".work-content",this).hide()
+	workBoxSizeSet()
+})
